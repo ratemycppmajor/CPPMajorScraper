e5764b120ae6db1d3b5993c05c5032375c126103
Brandon Diep <brandondiep2711@gmail.com>
2025-12-03 13:58:20 -0800

    update prisma to v7

--- scraper/scrapeCPP.ts
+++ scraper/scrapeCPP.ts
@@ -1,10 +1,15 @@
+import "dotenv/config";
 import puppeteer from "puppeteer";
 import fs from "fs";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { parseAvgGPA, calculateAverageGPAForMajor } from "./parseAvgGPA";
-const prisma = new PrismaClient();
 
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL 
+});
 
+const prisma = new PrismaClient({ adapter });
 
 /**
  * Helper function to clean major names based on your rules
